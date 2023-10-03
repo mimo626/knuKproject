68f0474246ea6af6be5b8689bd1e18beaae4439d
ub775 <kmgni@naver.com>
2023-10-03 15:40:29 +0900

    Design: 람브 사진 업데이트

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,36 @@
+import styled from "styled-components";
+import Header from "../components/Header";
+
+const Container = styled.div`
+    display: flex;
+`;
+
+const Content = styled.div`
+    width: 100%;
+    justify-content: center;
+`;
+
 function NotFound() {
+    const divStyle = {
+        marginLeft: "auto",
+        marginRight: "auto",
+        display: "block",
+    }
+
     return (
-        <div>
-            Not Found
-        </div>
+        <Container>
+            <Header />
+            <Content>
+                <div style={divStyle}>
+                    <img src="img/ramb_2.png" />
+                    <p>
+                        <strong>
+                            원하시는 페이지를 찾을 수 없습니다.
+                        </strong>
+                    </p>
+                </div>
+            </Content>
+        </Container>
     )
 }
 
