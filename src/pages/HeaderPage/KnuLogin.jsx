8117853b99247fb716ddeca1673e966de2e46a52
mimo626 <yessuk5499@gmail.com>
2023-11-30 00:28:39 +0900

    강남대 메일 인증 페이지

--- src/pages/HeaderPage/KnuLogin.jsx
+++ src/pages/HeaderPage/KnuLogin.jsx
@@ -1,7 +1,9 @@
+import knuLogo from "../../components/knuLogo.png";
 import Header from "../../components/Header";
 import axios from "axios";
 import React, { useState } from 'react';
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Page = styled.div`
     width:100%;
@@ -20,16 +22,27 @@ const Content = styled.div`
     background: #E8F9FA;
     box-shadow: 4px 4px 4px 0px rgba(79, 79, 79, 0.25);
 `;
+const TitleWrapper = styled.div`
+    display: inline-flex;
+    flex-direction: row;
+    gap: 3rem;
+    margin-left: 4.5rem;
+    padding-top: 1rem;
+`;
+const KnuLogo = styled.img`
+    width: 15rem;
+    height:15rem;
+    opacity: 40%;
+`;
 const TextWrapper = styled.div`
     display: inline-flex;
     flex-direction: column;
-    align-items: flex-start;
     gap: 1.1875rem;
-    margin: 0 auto;
     padding-top: 5rem;
+    padding-bottom: 4.5rem;
 `;
 const LoginTitle = styled.div`
-    color: black;
+    color: #7D7D7D;
     font-family: Inter;
     font-size: 2.1875rem;
     font-style: normal;
@@ -45,48 +58,82 @@ const LoginComment = styled.div`
     line-height: 100%;
 `;
 const TextWrapper2 = styled.div`
-    display: inline-flex;
+    display: flex;
     flex-direction: column;
-    align-items: flex-end;
-    gap: 3.25rem;
-    margin:0 auto;
+    gap: 1rem;
     width: 80%;
+    margin: 0 auto;
 `;
 const EmailWrapper = styled.div`
     display: inline-flex;
     flex-direction: row;
-    align-items: flex-end;
-    gap: 3.25rem;
-    margin:0 auto;
-    
+    gap: 2rem;
+    margin-bottom: 3rem;
+
 `;
 const KnuEmail= styled.div`
-    color: #AFAFAF;
-    text-align: center;
+    color: #7D7D7D;
     font-family: Inter;
     font-size: 1.25rem;
     font-style: normal;
-    font-weight: 500;
+    font-weight: 600;
     line-height: 100%;
 `;
-const EmailBtn = styled.div`
+const EmailInput = styled.input`
     display: flex;
-    width: 7.5rem;
+    width: 25rem;
     height: 2.5rem;
+    flex-shrink: 0;
+    background: #E8F9FA;
+    color: #AFAFAF;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    border-bottom: 0.1rem solid #AFAFAF;
+    &:focus{
+        outline: none;
+    }
+`;
+const EmailBtn = styled.button`
+    display: flex;
+    width: 10.9rem;
+    height: 2.85rem;
     padding: 0.75rem 2.5rem;
-    justify-content: center;
-    align-items: center;
     gap: 0.625rem;
     flex-shrink: 0;
+    justify-content: center;
+    align-items: center;
+    border-radius: 6.25rem;
+    border: 1px solid #006CBF;
+    background: #E8F9FA;
+    color: #006CBF;
+    &:hover {
+        font-weight: 600;
+        transition: all 0.3s;
+        cursor: pointer;
+    }
 `;
-const EmailText = styled.div`
-    color: var(--knu, #006CBF);
-    text-align: center;
-    font-family: Inter;
-    font-size: 0.9375rem;
-    font-style: normal;
+const MainBtn = styled.button`
+    display: flex;
+    width: 30rem;
+    height: 2.85rem;
+    padding: 0.75rem 2.5rem;
+    gap: 0.625rem;
+    flex-shrink: 0;
+    justify-content: center;
+    align-items: center;
+    border-radius: 6.25rem;
+    background: #006CBF;
+    color: #fff;
+    border: none;
+    margin: 0 auto;
+    font-size: 1rem;
     font-weight: 500;
-    line-height: 100%
+    &:hover {
+        font-weight: 700;
+        transition: all 0.3s;
+        cursor: pointer;
+    }
 `;
 function Login(){
   const [email, setEmail] = useState('');
@@ -118,30 +165,39 @@ function Login(){
   };
 
   return (
-    <div>
-      <label>
-        이메일:
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </label>
-      <button onClick={sendMail}>인증번호 발송</button>
-
-
-      <label>
-        인증번호:
-        <input type="text" value={enteredNumber} onChange={(e) => setEnteredNumber(e.target.value)} />
-      </label>
-      <button onClick={confirmNumber}>인증번호 확인</button>
-
-      <p>{message}</p>
-    </div>
+    <Page>
+            <Header></Header>
+            <Content>
+                <TitleWrapper>
+                    <TextWrapper>
+                        <LoginTitle>강남대학교 학생 인증</LoginTitle>
+                        <LoginComment>인증을 통해 더 많은 기능을 이용해보세요!</LoginComment>
+                    </TextWrapper>  
+                    <KnuLogo src={knuLogo}></KnuLogo>
+                </TitleWrapper>
+                <TextWrapper2>
+                    <KnuEmail>강남대 이메일</KnuEmail>
+                    <EmailWrapper>
+                        <EmailInput placeholder="ex) 202104126@kangnam.ac.kr" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <EmailBtn onClick={sendMail}>인증번호 발송</EmailBtn>
+                    </EmailWrapper>
+                </TextWrapper2>
+                <TextWrapper2>
+                    <KnuEmail>인증 번호</KnuEmail>
+                    <EmailWrapper>
+                        <EmailInput  type="email" value={enteredNumber} onChange={(e) => setEmail(e.target.value)} />
+                        <EmailBtn onClick={confirmNumber}>이메일 인증</EmailBtn>
+                    </EmailWrapper>
+                </TextWrapper2>
+                <TextWrapper2>
+                    <KnuEmail>본인의 학과</KnuEmail>
+                    <EmailWrapper>
+                        <EmailInput  type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </EmailWrapper>
+                </TextWrapper2>
+                <Link to={'/main'} style={{ textDecoration: "none"}}><MainBtn>인증 완료</MainBtn></Link>
+            </Content>
+        </Page>
   );
-
-
-    }
-    
-
-    
-    
-        
-        
-    export default Login;
+} 
+export default Login;
