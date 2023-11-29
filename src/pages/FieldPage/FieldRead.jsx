4f08901186fc555e92643b811fcf46fda9156bb6
kiwi1020 <108050749+kiwi1020@users.noreply.github.com>
2023-11-30 01:39:30 +0900

    1. 댓글 수정, 삭제 연동 2. 좋아요 연동 3. 에러 핸들링 4. 학부/선택 undefined 처리

--- src/pages/FieldPage/FieldRead.jsx
+++ src/pages/FieldPage/FieldRead.jsx
@@ -1,24 +1,24 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { createContext, useEffect, useState } from "react";
-import { json, Link, useParams } from "react-router-dom";
+import { json, Link, redirect, useParams } from "react-router-dom";
 
 function FieldRead() {
     const [data, setData] = useState([]);
+    const [userId, setUserId] = useState();
     const { noticeId } = useParams();
-  
+
     useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.post('/notice/requestbody', {
-            dbid: noticeId
-          });
-          setData(response.data);
-        } catch (error) {
-          console.error('에러가 발생했습니다:', error);
-        }
-      };
-  
-      fetchData();
+        const fetchData = async () => {
+            try {
+                const responseBody = await axios.post('/notice/requestbody', {
+                    dbid: noticeId
+                });
+                setData(responseBody.data);
+            } catch (error) {
+                console.error('에러가 발생했습니다:', error);
+            }
+        };
+        fetchData();
     }, []);
 
     return (
@@ -56,24 +56,66 @@ function FieldRead() {
             </table>
             {data.comments && data.comments.map(item => (
                 <table key={item.id}>
-                    <thead>
-                        <tr>
-                            <td>email</td>
-                            <td>comment</td>
-                            <td>createdDate</td>
-                            <td>modifiedDate</td>
-                        </tr>
-                    </thead>
                     <tbody>
                         <tr>
                             <td>{item.email}</td>
                             <td>{item.comment}</td>
                             <td>{item.createdDate}</td>
                             <td>{item.modifiedDate}</td>
+                            <td><button onClick={async () => {
+                                try {
+                                    await axios.post('/comments/delete', {
+                                        commentId: item.id
+                                    });
+                                    location.reload();
+                                } catch (error) {
+                                    alert(error.response.data)
+                                    console.error('에러가 발생했습니다:', error);
+                                }
+                            }} >댓글 삭제</button></td>
+
+                            <td><button onClick={async () => {
+                                try {
+                                    await axios.post('/comments/modify', {
+                                        commentId: item.id,
+                                        comment: prompt("댓글 수정")
+                                    });
+                                    location.reload();
+                                } catch (error) {
+                                    alert(error.response.data)
+                                    console.error('에러가 발생했습니다:', error);
+                                }
+                            }} >댓글 수정</button></td>
                         </tr>
                     </tbody>
                 </table>
             ))}
+            <textarea placeholder="댓글을 입력하세요" id="comments" cols="50" rows="5"></textarea>
+            <button onClick={async () => {
+                try {
+                    await axios.post('/comments/save', {
+                        noticeId: data.dbid,
+                        comment: document.getElementById("comments").value,
+                        noticeTitle: data.title
+                    });
+                    location.reload();
+                } catch (error) {
+                    alert(error.response.data)
+                    console.error('에러가 발생했습니다:', error);
+                }
+            }} >댓글 작성</button>
+            <br />
+            <button onClick={async () => {
+                try {
+                    await axios.post('/like/click', {
+                        noticeId: data.dbid,
+                    });
+                    location.reload();
+                } catch (error) {
+                    alert(error.response.data)
+                    console.error('에러가 발생했습니다:', error);
+                }
+            }}>{data.checkLike ? "좋아요 취소" : "좋아요"}</button>
         </div>
     );
 }
